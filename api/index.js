a183bcbc795dde60524d445974d45f2539532789
Aditya1-Sharma <pekodora6@gmail.com>
2024-03-15 15:56:50 +0530

    Deployment

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 const app = express();
-
+const  __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,12 +25,16 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
 
-const  __dirname = path.resolve();
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*' , (req , res)=>{
+  res.sendFile(path.join(__dirname , 'client' , 'dist' , 'index.html'))
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
